0f108edf3397a3ae05e07f1226e2f8984ccc50bc
Yanna <101520000+Yannastudent@users.noreply.github.com>
2022-07-07 14:18:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.css';
+import Welcome from './Welcome.js';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome  name="React router"/>
+      <nav>
+       <ul>
+       <li><Link to="/projects">Projects</Link> |{" "}</li>
+       <li><Link to="/about">About</Link></li>
+       <li><Link to="/home">Home</Link></li>
+       <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      </nav>
       </header>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path='home' element={<Home />} />
+      <Route path='contact' element={<Contact />} />
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Welcome.js
@@ -0,0 +1,6 @@
+import App from "./App"
+
+function Welcome(props) {
+    return <h1> Hello, {props.name} {props.Lastname}</h1>
+}
+export default Welcome;
